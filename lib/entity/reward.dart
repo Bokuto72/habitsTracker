9436c3db969575c6d90ea1da915ed9210f7ccf29
Thibault <t.lantzener@gmail.com>
2024-05-21 10:59:05 +0200

    ajout classes user et  reward

---
+++ lib/entity/reward.dart
@@ -0,0 +1,12 @@
+
+class Reward {
+  Reward({
+    this.id,
+    this.value,
+    this.name
+  });
+
+  final int? id;
+  final int? value;
+  final String? name;
+}
---
+++ lib/entity/user.dart
@@ -0,0 +1,18 @@
+
+import 'package:habits_tracker/entity/reward.dart';
+
+class User {
+  User({
+    this.pseudo,
+    this.level,
+    this.xp,
+    this.health,
+    this.rewards,
+  });
+
+  final String? pseudo;
+  final int? level;
+  final int? xp;
+  final int? health;
+  final List<Reward>? rewards;
+}
