b9f40e29cd586de371fe677db298908887d8a515
Thibault <t.lantzener@gmail.com>
2024-05-22 16:29:55 +0200

    début controllers et création classe utilitaire Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
+class Colors {
+  static const primaryColor = Color(0xffffffff);
+}
---
+++ lib/views/user_creation.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:habits_tracker/controllers/user_controller.dart';
+
+class UserCreation extends StatefulWidget {
+  const UserCreation({super.key});
+
+  @override
+  State<UserCreation> createState() => _UserCreationState();
+}
+
+class _UserCreationState extends State<UserCreation> {
+  UserController userController = UserController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
