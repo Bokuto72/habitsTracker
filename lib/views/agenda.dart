c180f10d88b402b5a525a416fde2add840b0137f
Thibault <t.lantzener@gmail.com>
2024-05-27 12:23:21 +0200

    ajout d'un calendrier. Pas convaincu par la lib table_calendar qui ne permet pas de voir au format quotidien, peut être voir avec calendar_view.

--- lib/views/agenda.dart
+++ lib/views/agenda.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class AgendaPage extends StatefulWidget {
   const AgendaPage({super.key});
@@ -8,9 +10,30 @@ class AgendaPage extends StatefulWidget {
 }
 
 class _AgendaPageState extends State<AgendaPage> {
+  DateTime today = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Text("Feur.");
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Agenda")
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        Container(
+          child: TableCalendar(
+            focusedDay: today,
+            firstDay: DateTime.utc(2015, 1, 1),
+            lastDay: DateTime.utc(2030, 1, 1),
+            calendarFormat: CalendarFormat.,
+          ),
+        )
+      ],
+    );
   }
 }
